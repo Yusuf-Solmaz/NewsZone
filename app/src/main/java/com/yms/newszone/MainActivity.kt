e19b545b186ce9a9c596057a803d71c300ae5655
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-11 19:33:20 +0300

    add and test first composable

--- app/src/main/java/com/yms/newszone/MainActivity.kt
+++ app/src/main/java/com/yms/newszone/MainActivity.kt
@@ -5,14 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.yms.newszone.ui.theme.NewsZoneTheme
+import com.yms.presentation.navigation.TestPage
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NewsZoneTheme {
-
+                TestPage()
             }
         }
     }
