f31c4e91c64d5f63c58bd8758e446858ff3dddf5
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-12 22:57:31 +0300

    add DataStore and UserPreferencesRepository injections

---
+++ data/src/main/java/com/yms/data/di/DataStoreModule.kt
@@ -0,0 +1,33 @@
+package com.yms.data.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.yms.data.repository.UserPreferencesRepositoryImpl
+import com.yms.data.utils.USER_PREFERENCES
+import com.yms.domain.repository.UserPreferencesRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = USER_PREFERENCES)
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return context.dataStore
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserPreferencesRepository(dataStore: DataStore<Preferences>): UserPreferencesRepository {
+        return UserPreferencesRepositoryImpl(dataStore)
+    }
+}
