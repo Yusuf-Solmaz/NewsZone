6cf43873733056721f3d8626b27528498e08e9cc
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-14 15:20:46 +0300

    feat: add category

--- data/src/main/java/com/yms/data/utils/Constants.kt
+++ data/src/main/java/com/yms/data/utils/Constants.kt
@@ -2,3 +2,4 @@ package com.yms.data.utils
 
 const val USER_PREFERENCES = "user_preferences"
 const val APP_ENTRY_PREFERENCES = "app_entry_preferences"
+const val CATEGORY_PREFERENCES = "category_preferences"
