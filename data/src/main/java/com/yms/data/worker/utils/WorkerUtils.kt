41fb79244b7d3206f2cc5f9631e9dfb6b024b8dc
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-14 22:44:24 +0300

    feat: add required work manager const values

---
+++ data/src/main/java/com/yms/data/worker/utils/WorkerUtils.kt
@@ -0,0 +1,7 @@
+package com.yms.data.worker.utils
+
+const val FOLLOW_UP_TIME_KEY = "follow_up_time"
+const val AGE_GROUP_KEY = "age_group"
+const val GENDER_KEY = "gender"
+const val WORKER_ERROR = "worker_error"
+const val OUTPUT_CATEGORY_KEY = "output_category_key"
