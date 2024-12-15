fa9168a2e905ae2d260963bd96e99ac77bd48c3b
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-15 19:04:00 +0300

    feat: add BASE_URL

--- data/src/main/java/com/yms/data/utils/Constants.kt
+++ data/src/main/java/com/yms/data/utils/Constants.kt
@@ -3,3 +3,4 @@ package com.yms.data.utils
 const val USER_PREFERENCES = "user_preferences"
 const val APP_ENTRY_PREFERENCES = "app_entry_preferences"
 const val CATEGORY_PREFERENCES = "category_preferences"
+const val BASE_URL = "https://newsapi.org/v2/"
