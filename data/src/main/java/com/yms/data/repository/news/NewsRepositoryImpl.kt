57bedd84f201a31854b44791bab1d0d4abb5c163
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-15 19:08:13 +0300

    feat: complete getNewsByCategory and searchNews

---
+++ data/src/main/java/com/yms/data/repository/news/NewsRepositoryImpl.kt
@@ -0,0 +1,54 @@
+package com.yms.data.repository.news
+
+import android.util.Log
+import com.yms.data.mapper.NewsMapper.toDomain
+import com.yms.data.remote.NewsApi
+import com.yms.domain.model.news.NewsData
+import com.yms.domain.repository.news.NewsRepository
+import com.yms.domain.utils.RootResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class NewsRepositoryImpl @Inject constructor(val api: NewsApi): NewsRepository {
+    override fun getNewsByCategory(
+        category: String,
+        page: Int,
+        pageSize: Int
+    ): Flow<RootResult<NewsData>> = flow {
+        emit(RootResult.Loading)
+
+        try {
+            val response = api.getNewsByCategory(category = category, page = page, pageSize = pageSize)
+
+            Log.d("NewsRepositoryImpl", "getNewsByCategory: ${response.articleDtos?.size}")
+
+            emit(RootResult.Success(response.toDomain()))
+
+            Log.d("NewsRepositoryImpl", "getNewsByCategory: ${response.toDomain().articleDtos.size}")
+        }
+        catch (e:Exception){
+            emit(RootResult.Error(e.message.toString()))
+        }
+    }.flowOn(Dispatchers.IO)
+
+    override fun searchNews(
+        query: String,
+        sortBy: String?,
+        page: Int,
+        pageSize: Int
+    ): Flow<RootResult<NewsData>> = flow {
+        emit(RootResult.Loading)
+        try {
+            val response = api.searchNews(query = query, sortBy = sortBy, page = page, pageSize = pageSize)
+
+            emit(RootResult.Success(response.toDomain()))
+        }
+        catch (e:Exception){
+            emit(RootResult.Error(e.message.toString()))
+        }
+    }.flowOn(Dispatchers.IO)
+
+}
---
+++ domain/src/main/java/com/yms/domain/repository/news/NewsRepository.kt
@@ -0,0 +1,11 @@
+package com.yms.domain.repository.news
+
+import com.yms.domain.model.news.NewsData
+import com.yms.domain.utils.RootResult
+import kotlinx.coroutines.flow.Flow
+
+interface NewsRepository {
+    fun getNewsByCategory(category: String, page: Int, pageSize: Int): Flow<RootResult<NewsData>>
+    fun searchNews(query: String, sortBy: String?, page: Int, pageSize: Int): Flow<RootResult<NewsData>>
+
+}
