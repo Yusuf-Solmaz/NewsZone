5dcda3ab9418d884a5e273b734192a654f6c43e1
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-15 19:03:09 +0300

    feat: add getNewsByCategory and searchNews

---
+++ data/src/main/java/com/yms/data/remote/NewsApi.kt
@@ -0,0 +1,28 @@
+package com.yms.data.remote
+
+import com.yms.data.BuildConfig
+import com.yms.data.remote.dto.news.NewsRoot
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("top-headlines")
+    suspend fun getNewsByCategory(
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY,
+        @Query("category") category: String,
+        @Query("page") page: Int = 1,
+        @Query("pageSize") pageSize: Int = 20
+    ): NewsRoot
+
+
+    @GET("everything")
+    suspend fun searchNews(
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY,
+        @Query("q") query: String,
+        @Query("sortBy") sortBy: String? = "publishedAt",
+        @Query("page") page: Int = 1,
+        @Query("pageSize") pageSize: Int = 20
+    ): NewsRoot
+
+}
