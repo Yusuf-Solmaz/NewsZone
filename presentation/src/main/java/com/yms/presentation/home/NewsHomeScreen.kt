0ae83567113a377cbaa33a14e9e6c227d49b046f
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-15 19:09:45 +0300

    feat: add view model  to home screen

--- presentation/src/main/java/com/yms/presentation/home/NewsHomeScreen.kt
+++ presentation/src/main/java/com/yms/presentation/home/NewsHomeScreen.kt
@@ -1,5 +1,6 @@
 package com.yms.presentation.home
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,8 +17,11 @@ import com.yms.presentation.home.viewmodel.NewsHomeViewModel
 fun NewsHomeScreen(viewModel: NewsHomeViewModel = hiltViewModel()){
 
     val categoryState by viewModel.categoryState.collectAsState()
+    val newsState by viewModel.newsState.collectAsState()
 
-    if (categoryState.isLoading){
+    Log.d("NewsHomeScreen", "NewsHomeScreen: ${newsState.news}")
+
+    if (newsState.isLoading){
         Column(
             Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -31,7 +35,10 @@ fun NewsHomeScreen(viewModel: NewsHomeViewModel = hiltViewModel()){
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            Text(categoryState.category)
+            //Text(categoryState.category)
+
+            Text(newsState.news.size.toString())
+
         }
     }
 }
