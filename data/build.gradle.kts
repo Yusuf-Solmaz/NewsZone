d8bfa10ad172b0691319f701a1d9eff807e507b1
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-15 11:51:06 +0300

    docs: add work manager and hilt dependency

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -74,4 +74,7 @@ dependencies {
     implementation(libs.androidx.datastore.preferences)
 
     implementation(libs.androidx.work.runtime.ktx)
+
+    implementation(libs.hilt.android.work)
+    ksp(libs.hilt.androidx.compiler)
 }
