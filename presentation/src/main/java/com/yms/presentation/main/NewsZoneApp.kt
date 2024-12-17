b1e8235370414c3f412aa2584b712e7426888042
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-17 18:45:35 +0300

    feat: add padding

--- presentation/src/main/java/com/yms/presentation/main/NewsZoneApp.kt
+++ presentation/src/main/java/com/yms/presentation/main/NewsZoneApp.kt
@@ -1,16 +1,17 @@
 package com.yms.presentation.main
 
-import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -20,34 +21,45 @@ import com.yms.presentation.navigation.NewsZoneNavigation
 import com.yms.presentation.onboarding.viewmodel.OnBoardingViewModel
 import com.yms.theme.NewsZoneTheme
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+
 @Composable
-fun NewsZoneApp(viewModel: OnBoardingViewModel = hiltViewModel(),navController: NavHostController = rememberNavController()){
+fun NewsZoneApp(
+    viewModel: OnBoardingViewModel = hiltViewModel(),
+    navController: NavHostController = rememberNavController()
+) {
 
     val backStackEntry by navController.currentBackStackEntryAsState()
     val currentScreen = NavigationGraph.valueOf(
         backStackEntry?.destination?.route ?: NavigationGraph.NEWS_HOME.name
     )
 
+
     val onBoardingState by viewModel.uiState.collectAsState()
+    val layoutDirection = LocalLayoutDirection.current
 
     Log.d("NewsZoneApp", "Current Screen: $onBoardingState")
 
     NewsZoneTheme {
-        Scaffold(
-            content = {
-                Column(
-                    modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.primary)
-                ) {
-                    NewsZoneNavigation(
-                        navController = navController,
-                        saveAppEntry = viewModel::onEvent,
-                        onBoardingState = onBoardingState
-                    )
-                }
+            Scaffold(
+                content = {
+                    paddingValues->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.background)
+                            .padding(
+                                paddingValues
+                            )
+                    ) {
+                        NewsZoneNavigation(
+                            navController = navController,
+                            saveAppEntry = viewModel::onEvent,
+                            onBoardingState = onBoardingState
+                        )
+                    }
 
-            }
-        )
-    }
+                }
+            )
+        }
 
 }
