d30da72099e883428f88aa93aa3368161b95c648
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-17 18:43:44 +0300

    feat: add SerializedName annotation

--- data/src/main/java/com/yms/data/remote/dto/news/SourceDto.kt
+++ data/src/main/java/com/yms/data/remote/dto/news/SourceDto.kt
@@ -1,6 +1,10 @@
 package com.yms.data.remote.dto.news
 
+import com.google.gson.annotations.SerializedName
+
 data class SourceDto(
+    @SerializedName("id")
     val id: String?,
+    @SerializedName("name")
     val name: String?
 )
