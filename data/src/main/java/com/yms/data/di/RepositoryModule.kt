5129c70faff2ba994bee28d62ee032df29d1ffa7
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-19 14:22:39 +0300

    feat: add database to NewsRepository module

--- data/src/main/java/com/yms/data/di/RepositoryModule.kt
+++ data/src/main/java/com/yms/data/di/RepositoryModule.kt
@@ -2,9 +2,10 @@ package com.yms.data.di
 
 import android.content.Context
 import androidx.work.WorkManager
+import com.yms.data.local.db.NewsDatabase
 import com.yms.data.remote.NewsApi
 import com.yms.data.repository.customization.CustomizationRepositoryImpl
-import com.yms.data.repository.news.NewsRepositoryImpl
+import com.yms.data.repository.remote.news.NewsRepositoryImpl
 import com.yms.domain.repository.news.NewsRepository
 import com.yms.domain.repository.user_preferences.CustomizationRepository
 import dagger.Module
@@ -29,7 +30,7 @@ object RepositoryModule {
 
     @Provides
     @Singleton
-    fun provideNewsRepository(api:NewsApi): NewsRepository {
-        return NewsRepositoryImpl(api)
+    fun provideNewsRepository(api:NewsApi,newsDatabase: NewsDatabase): NewsRepository {
+        return NewsRepositoryImpl(api,newsDatabase)
     }
 }
