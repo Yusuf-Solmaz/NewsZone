d01f8714fe2da60f753e619a4c43f7c367977a59
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-19 14:23:17 +0300

    feat: add pagination get methods

--- domain/src/main/java/com/yms/domain/repository/news/NewsRepository.kt
+++ domain/src/main/java/com/yms/domain/repository/news/NewsRepository.kt
@@ -1,11 +1,18 @@
 package com.yms.domain.repository.news
 
+import androidx.paging.PagingData
+import com.yms.domain.model.news.ArticleData
 import com.yms.domain.model.news.NewsData
 import com.yms.domain.utils.RootResult
 import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
+    fun getPagedNewsByCategory(category: String?): Flow<PagingData<ArticleData>>
+    fun searchPagedNews(query: String): Flow<PagingData<ArticleData>>
+
     fun getNewsByCategory(category: String?, page: Int, pageSize: Int, source: String?): Flow<RootResult<NewsData>>
     fun searchNews(query: String, sortBy: String?, page: Int, pageSize: Int): Flow<RootResult<NewsData>>
+
+    fun getNewsWithMediator(category: String?): Flow<PagingData<ArticleData>>
 
 }
