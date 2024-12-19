f79f46559bd9cd6747b0e502d98722f75dbb8d1e
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-19 14:24:58 +0300

    feat: update lazy paging items

--- presentation/src/main/java/com/yms/presentation/home/NewsHomeScreen.kt
+++ presentation/src/main/java/com/yms/presentation/home/NewsHomeScreen.kt
@@ -1,6 +1,7 @@
 package com.yms.presentation.home
 
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -15,8 +16,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ScrollableTabRow
@@ -32,63 +33,73 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.SubcomposeAsyncImage
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.rememberLottieComposition
+import com.yms.domain.model.news.ArticleData
 import com.yms.presentation.R
 import com.yms.presentation.home.content.BreakingNewsSection
 import com.yms.presentation.home.viewmodel.NewsHomeViewModel
-import com.yms.presentation.home.viewmodel.NewsState
+
 
 @Composable
-fun NewsHomeScreen(modifier: Modifier = Modifier, viewModel: NewsHomeViewModel = hiltViewModel()) {
-
+fun NewsHomeScreen(
+    modifier: Modifier = Modifier,
+    viewModel: NewsHomeViewModel = hiltViewModel()
+) {
     val categoryState by viewModel.categoryState.collectAsState()
+    val pagedNews = viewModel.pagedNews.collectAsLazyPagingItems()
 
-    val newsState by viewModel.newsState.collectAsStateWithLifecycle()
-    val breakingNewsState by viewModel.breakingNewsState.collectAsStateWithLifecycle()
+    val context = LocalContext.current
 
-    Log.d("NewsHomeScreen", "NewsHomeScreen: ${newsState.news}")
-
-    if (categoryState.isLoading) {
-
-    }else{
-        LaunchedEffect(categoryState.category) {
-            viewModel.getNewsByCategory(categoryState.category, page = 1, pageSize = 20)
+    LaunchedEffect(key1 = pagedNews.loadState) {
+        if (pagedNews.loadState.refresh is LoadState.Error) {
+            Toast.makeText(
+                context,
+                "Error: " + (pagedNews.loadState.refresh as LoadState.Error).error.message,
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
+
+    if (!categoryState.isLoading) {
+        LaunchedEffect(categoryState.category) {
+            viewModel.getPagedNewsWithMediator(categoryState.category)
+        }
+    }
+
     Column(
         modifier = modifier
             .fillMaxSize()
             .padding(dimensionResource(R.dimen.padding_medium))
             .background(MaterialTheme.colorScheme.background)
     ) {
+        BreakingNewsSection(breakingNewsState = viewModel.breakingNewsState.collectAsState().value)
 
-
-        BreakingNewsSection(breakingNewsState = breakingNewsState)
-
-        ScrollableTabRowExample(onTabSelected = {
-            category ->
-            viewModel.getNewsByCategory(category = category, page = 1, pageSize = 20)
-        }, newsState = newsState, category = categoryState.category)
+        ScrollableTabRowExample(onTabSelected = { category ->
+            viewModel.getPagedNewsByCategory(category)
+        }, pagedNews = pagedNews, category = categoryState.category)
     }
-
 }
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScrollableTabRowExample(
     onTabSelected: (String) -> Unit,
     modifier: Modifier = Modifier,
-    newsState: NewsState,
+    pagedNews: LazyPagingItems<ArticleData>,
     category: String
 ) {
     val tabTitles = listOf(
@@ -102,8 +113,6 @@ fun ScrollableTabRowExample(
         selectedTabIndex.value = initialIndex
     }
 
-
-
     Column {
         ScrollableTabRow(
             selectedTabIndex = selectedTabIndex.value,
@@ -111,14 +120,20 @@ fun ScrollableTabRowExample(
             edgePadding = 8.dp
         ) {
             tabTitles.forEachIndexed { index, title ->
-
                 Tab(
                     selected = selectedTabIndex.value == index,
                     onClick = {
                         selectedTabIndex.value = index
                         onTabSelected(title)
                     },
-                    text = { Text(text = title,style = MaterialTheme.typography.titleSmall.copy(color = MaterialTheme.colorScheme.primary)) }
+                    text = {
+                        Text(
+                            text = title,
+                            style = MaterialTheme.typography.titleSmall.copy(
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                        )
+                    }
                 )
             }
         }
@@ -126,27 +141,24 @@ fun ScrollableTabRowExample(
         Spacer(modifier = Modifier.height(16.dp))
 
         LazyColumn {
-            items(newsState.news?.size ?: 0) { index ->
-                val article = newsState.news?.get(index)
-                Log.d("NewsHomeScreen", "NewsHomeScreen: ${article?.urlToImage}")
 
-                if (article?.urlToImage != null &&
-                    article.urlToImage != "null" &&
-                    article.urlToImage != "Data not available") {
+            items(pagedNews.itemCount) { index ->
+                val article = pagedNews[index]
 
+                if (article != null) {
+                    Log.d("NewsHomeScreen", "Article: ${article.urlToImage}")
                     NewsCard(
                         imageUrl = article.urlToImage,
                         title = article.title,
                         author = article.author,
                         date = article.timeAgo
                     )
-
-                    HorizontalDivider(modifier = Modifier.fillMaxWidth())
                 }
-
-
-
-
+            }
+            item {
+                if (pagedNews.loadState.append is LoadState.Loading) {
+                    CircularProgressIndicator()
+                }
             }
         }
     }
@@ -170,70 +182,86 @@ fun NewsCard(
             .background(MaterialTheme.colorScheme.background),
         shape = MaterialTheme.shapes.medium
     ) {
-        Row(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
 
-            SubcomposeAsyncImage(
-                model = imageUrl,
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .width(120.dp)
-                    .padding(end = dimensionResource(R.dimen.padding_small))
-                    .clip(MaterialTheme.shapes.medium),
-                contentScale = ContentScale.Crop,
-                loading = {
-                    LottieAnimation(
-                        composition,
-                        modifier = Modifier.size(100.dp),
-                        iterations = Int.MAX_VALUE
-                    )
-                }
-            )
 
-            Column(modifier = Modifier.weight(1f).fillMaxHeight(), verticalArrangement = Arrangement.SpaceBetween) {
-                Text(
-                    text = title,
-                    style = MaterialTheme.typography.titleMedium,
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis,
-                    modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_medium))
+            Row(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+
+                SubcomposeAsyncImage(
+                    model = imageUrl,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .width(120.dp)
+                        .padding(end = dimensionResource(R.dimen.padding_small))
+                        .clip(MaterialTheme.shapes.medium),
+                    contentScale = ContentScale.Crop,
+                    loading = {
+                        LottieAnimation(
+                            composition,
+                            modifier = Modifier.size(100.dp),
+                            iterations = Int.MAX_VALUE
+                        )
+                    }
                 )
-                Spacer(modifier = Modifier.height(4.dp))
-                Row (modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_small)), verticalAlignment = Alignment.CenterVertically){
+
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight(),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
                     Text(
-                        text = author,
-                        style = MaterialTheme.typography.labelSmall,
-                        color = MaterialTheme.colorScheme.primary
+                        text = title,
+                        style = MaterialTheme.typography.titleMedium,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_medium))
                     )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_dot),
-                        contentDescription = "Dot Icon",
-                        tint = MaterialTheme.colorScheme.onBackground,
-                        modifier = Modifier.size(5.dp)
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(
-                        text = date,
-                        style = MaterialTheme.typography.labelSmall,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant
-                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Row(
+                        modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_small)),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = author,
+                            style = MaterialTheme.typography.labelSmall,
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_dot),
+                            contentDescription = "Dot Icon",
+                            tint = MaterialTheme.colorScheme.onBackground,
+                            modifier = Modifier.size(5.dp)
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(
+                            text = date,
+                            style = MaterialTheme.typography.labelSmall,
+                            color = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    }
                 }
+
             }
+
         }
-    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun NewsCardPreview() {
-    Column(modifier = Modifier.fillMaxWidth().height(100.dp)) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        ) {
         NewsCard(
             imageUrl = "https://static.foxnews.com/foxnews.com/content/uploads/2024/12/james-lankford.jpg",
             title = "Sample News Title",
             author = "Author Name",
             date = "2023-09-01"
         )
+
     }
 
 }
