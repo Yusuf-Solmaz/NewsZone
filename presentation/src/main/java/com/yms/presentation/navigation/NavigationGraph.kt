0ccd8f1e84788c500caaf99b395306ed179ea68f
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-22 07:32:32 +0300

    feat: add detail and main screen navigation graph

--- presentation/src/main/java/com/yms/presentation/navigation/NavigationGraph.kt
+++ presentation/src/main/java/com/yms/presentation/navigation/NavigationGraph.kt
@@ -7,8 +7,9 @@ enum class NavigationGraph(@StringRes val title: Int?){
     ONBOARDING_SCREEN(null),
     SPLASH_SCREEN(null),
     CUSTOMIZATION_SCREEN(null),
+    MAIN_CONTENT(null),
+    ARTICLE_DETAIL_SCREEN(null),
     NEWS_HOME(R.string.home),
     SEARCH_SCREEN(R.string.search),
     SETTINGS_SCREEN(R.string.settings)
-
 }
