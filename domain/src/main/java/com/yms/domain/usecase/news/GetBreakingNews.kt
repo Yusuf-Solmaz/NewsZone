fe6b204523a252247283711deb0ed32fffa26c9f
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-22 07:32:11 +0300

    feat: update package

--- domain/src/main/java/com/yms/domain/usecase/news/GetBreakingNews.kt
+++
@@ -1,8 +0,0 @@
-package com.yms.domain.usecase.news
-
-
-import com.yms.domain.repository.news.NewsRepository
-
-class GetBreakingNews (val newsRepository: NewsRepository){
-    operator fun invoke(page: Int, pageSize: Int) = newsRepository.getNewsByCategory(category = null ,page = 1, pageSize, source = "cnn")
-}
