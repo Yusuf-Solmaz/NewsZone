23ce2cb291bae489208287e7bb02dea9c8c71788
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-22 07:38:05 +0300

    feat: delete use case

--- domain/src/main/java/com/yms/domain/usecase/news/SearchNews.kt
+++
@@ -1,7 +0,0 @@
-package com.yms.domain.usecase.news
-
-import com.yms.domain.repository.news.NewsRepository
-
-class SearchNews(val newsRepository: NewsRepository){
-    operator fun invoke(query: String, sortBy: String?, page: Int, pageSize: Int) = newsRepository.searchNews(query, sortBy, page, pageSize)
-}
