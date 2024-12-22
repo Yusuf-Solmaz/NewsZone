8b5abc1ddae9bcfa60bdee9312c9063e70c99608
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-22 07:37:13 +0300

    feat: add SharedViewModel

--- presentation/src/main/java/com/yms/presentation/navigation/NewsZoneNavigation.kt
+++ presentation/src/main/java/com/yms/presentation/navigation/NewsZoneNavigation.kt
@@ -6,11 +6,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.yms.presentation.article_detail.ArticleDetailScreen
 import com.yms.presentation.customization.CustomizationScreen
 import com.yms.presentation.home.NewsHomeScreen
 import com.yms.presentation.onboarding.OnBoardingScreen
@@ -19,6 +24,7 @@ import com.yms.presentation.onboarding.viewmodel.OnBoardingState
 import com.yms.presentation.search.SearchScreen
 import com.yms.presentation.settings.SettingsScreen
 import com.yms.presentation.splash.SplashScreen
+import com.yms.utils.SharedViewModel
 
 @Composable
 fun NewsZoneNavigation(
@@ -27,6 +33,8 @@ fun NewsZoneNavigation(
     saveAppEntry: (OnBoardingEvent) -> Unit,
     onBoardingState: OnBoardingState
 ) {
+
+    val sharedViewModel: SharedViewModel = hiltViewModel()
 
     if (onBoardingState.isSplashScreenVisible) {
         SplashScreen()
@@ -42,6 +50,41 @@ fun NewsZoneNavigation(
             navController = navController,
             startDestination = onBoardingState.startDestination
         ) {
+            navigation(
+                startDestination = NavigationGraph.NEWS_HOME.name,
+                route = NavigationGraph.MAIN_CONTENT.name
+            ){
+                composable(NavigationGraph.NEWS_HOME.name) {
+                    entry->
+
+                    NewsHomeScreen(
+                        navigateToSearchScreen = {
+                            navController.navigate(NavigationGraph.SEARCH_SCREEN.name)
+                        },
+                        navigateToArticleDetailScreen = {
+                            articleData ->
+                            sharedViewModel.updateState(articleData)
+                            navController.navigate(NavigationGraph.ARTICLE_DETAIL_SCREEN.name)
+                        }
+                    )
+                }
+                composable(NavigationGraph.SEARCH_SCREEN.name) {
+                    SearchScreen()
+                }
+                composable(NavigationGraph.ARTICLE_DETAIL_SCREEN.name){
+                    val state by sharedViewModel.sharedState.collectAsStateWithLifecycle()
+
+                    ArticleDetailScreen(
+                        sharedState = state
+                    )
+                }
+
+            }
+
+            composable(NavigationGraph.SETTINGS_SCREEN.name) {
+                SettingsScreen()
+            }
+
             composable(NavigationGraph.ONBOARDING_SCREEN.name) {
                 OnBoardingScreen(
                     modifier = Modifier.fillMaxSize(),
@@ -58,17 +101,6 @@ fun NewsZoneNavigation(
             }
             composable(NavigationGraph.SPLASH_SCREEN.name) {
                 SplashScreen()
-            }
-            composable(NavigationGraph.NEWS_HOME.name) {
-                NewsHomeScreen(
-                    navigateToSearchScreen = { navController.navigate(NavigationGraph.SEARCH_SCREEN.name) }
-                )
-            }
-            composable(NavigationGraph.SEARCH_SCREEN.name) {
-                SearchScreen()
-            }
-            composable(NavigationGraph.SETTINGS_SCREEN.name) {
-                SettingsScreen()
             }
         }
     }
