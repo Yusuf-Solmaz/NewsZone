9ebd9c5d1e253768534f0ddf5d99e2049693d06a
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-25 11:15:43 +0300

    feat: add delete article and is saved article event

--- presentation/src/main/java/com/yms/presentation/article_detail/ArticleDetailEvent.kt
+++ presentation/src/main/java/com/yms/presentation/article_detail/ArticleDetailEvent.kt
@@ -4,4 +4,6 @@ import com.yms.domain.model.news.ArticleData
 
 sealed interface ArticleDetailEvent {
     data class InsertArticle(val article: ArticleData) : ArticleDetailEvent
+    data class IsArticleSaved(val articleUrl: String) : ArticleDetailEvent
+    data class DeleteArticle(val articleUrl: String) : ArticleDetailEvent
 }
