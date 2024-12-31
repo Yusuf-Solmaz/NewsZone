e6dd315223516cf2ee2cc75876e81032e1a3a789
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-31 13:42:34 +0300

    feat: add notification repository with constraints

---
+++ data/src/main/java/com/yms/data/repository/notification/BreakingNewsNotificationRepositoryImpl.kt
@@ -0,0 +1,36 @@
+package com.yms.data.repository.notification
+
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.yms.data.worker.NotificationWorker
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+class BreakingNewsNotificationRepositoryImpl @Inject constructor(
+    private val workManager: WorkManager
+) {
+
+
+    fun scheduleBreakingNewsNotification() {
+        val workRequest = PeriodicWorkRequestBuilder<NotificationWorker>(
+            2, TimeUnit.HOURS
+        )
+            .setConstraints(
+                Constraints.Builder()
+                    .setRequiredNetworkType(NetworkType.CONNECTED)
+                    .setRequiresBatteryNotLow(true)
+                    .build()
+            )
+            .build()
+
+
+        workManager.enqueueUniquePeriodicWork(
+            NotificationWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.REPLACE,
+            workRequest
+        )
+    }
+}
