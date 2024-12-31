d6df2aff68784749057a33f6938cb538d52d6c73
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-31 13:44:47 +0300

    feat: add breaking news notification worker

--- app/src/main/java/com/yms/newszone/NewsZoneApplication.kt
+++ app/src/main/java/com/yms/newszone/NewsZoneApplication.kt
@@ -3,6 +3,7 @@ package com.yms.newszone
 import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import com.yms.data.repository.notification.BreakingNewsNotificationRepositoryImpl
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
@@ -11,6 +12,14 @@ class NewsZoneApplication: Application(), Configuration.Provider {
 
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
+
+    @Inject
+    lateinit var breakingNewsNotificationRepository: BreakingNewsNotificationRepositoryImpl
+
+    override fun onCreate() {
+        super.onCreate()
+        breakingNewsNotificationRepository.scheduleBreakingNewsNotification()
+    }
 
     override val workManagerConfiguration: Configuration
         get() = Configuration.Builder()
