002d7221829be4e586b1978e5f2f1039b19473f7
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-01-02 15:26:01 +0300

    feat: add required constants

--- data/src/main/java/com/yms/data/utils/Constants.kt
+++ data/src/main/java/com/yms/data/utils/Constants.kt
@@ -3,6 +3,8 @@ package com.yms.data.utils
 const val USER_PREFERENCES = "user_preferences"
 const val APP_ENTRY_PREFERENCES = "app_entry_preferences"
 const val CATEGORY_PREFERENCES = "category_preferences"
+const val DARK_MODE_PREFERENCES = "dark_mode_preferences"
+const val LANGUAGE_PREFERENCES = "language_preferences"
 const val BASE_URL = "https://newsapi.org/v2/"
 
 val VERBOSE_NOTIFICATION_CHANNEL_NAME: CharSequence =
