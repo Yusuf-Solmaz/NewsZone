7f3d451df76608eff71288e78a10dc35b6a0ac08
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-01-02 19:08:43 +0300

    feat: init commit splash screen

--- presentation/src/main/java/com/yms/presentation/splash/SplashScreen.kt
+++ presentation/src/main/java/com/yms/presentation/splash/SplashScreen.kt
@@ -4,22 +4,30 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen(modifier: Modifier = Modifier){
-    val scope = rememberCoroutineScope()
+fun SplashScreen(modifier: Modifier = Modifier, navigateToScreen: () -> Unit) {
+
+    LaunchedEffect(true) {
+        delay(2500)
+        navigateToScreen()
+    }
+
     Column(
-        modifier = modifier.fillMaxSize().background(MaterialTheme.colorScheme.background),
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Black),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
-        Text(text = "Splash Screen")
+        Text(text = "Splash Screen", color = Color.White)  // Ekranda yazıyı göster
     }
+
 }
