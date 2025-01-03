ae0841df0136ae4c8daa37234632b174f178d5b3
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-01-03 19:39:17 +0300

    feat: add get summary and summary state

--- presentation/src/main/java/com/yms/utils/SharedViewModel.kt
+++ presentation/src/main/java/com/yms/utils/SharedViewModel.kt
@@ -1,26 +1,66 @@
 package com.yms.utils
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.yms.domain.model.news.BaseArticle
+import com.yms.domain.usecase.gemini.SummaryUseCase
+import com.yms.domain.utils.RootResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SharedViewModel @Inject constructor(
-): ViewModel() {
+class SharedViewModel @Inject constructor(val summaryUseCase: SummaryUseCase) : ViewModel() {
 
     private val _sharedArticleState = MutableStateFlow<SharedArticleState>(SharedArticleState())
     val sharedArticleState: StateFlow<SharedArticleState>
         get() = _sharedArticleState
 
-    fun updateState(article: BaseArticle? = null){
-        try {
-            _sharedArticleState.value = _sharedArticleState.value.copy(article = article,isLoading = false,error = null)
+    private val _summaryState = MutableStateFlow<SummaryState>(SummaryState.Idle)
+    val summaryState: StateFlow<SummaryState>
+        get() = _summaryState
+
+    fun onEvent(event: SummaryEvent) {
+        when (event) {
+            is SummaryEvent.GetSummary -> {
+                getSummary(event.content)
+            }
         }
-        catch (e:Exception){
-            _sharedArticleState.value = _sharedArticleState.value.copy(isLoading = false,error = e.message)
+    }
+
+    private fun getSummary(content: String) {
+        _summaryState.value = SummaryState.Loading
+
+        viewModelScope.launch {
+            summaryUseCase.getSummary(content).collect {
+                result ->
+                when (result) {
+                    is RootResult.Success -> {
+                        _summaryState.value = SummaryState.Success(result.data?.summary ?: "Something went wrong")
+                    }
+                    is RootResult.Error -> {
+                        _summaryState.value = SummaryState.Error(result.message)
+                    }
+                    is RootResult.Loading -> {
+                        _summaryState.value = SummaryState.Loading
+                    }
+                }
+            }
+        }
+    }
+
+    fun updateState(article: BaseArticle? = null) {
+        try {
+            _sharedArticleState.value = _sharedArticleState.value.copy(
+                article = article,
+                isLoading = false,
+                error = null
+            )
+        } catch (e: Exception) {
+            _sharedArticleState.value =
+                _sharedArticleState.value.copy(isLoading = false, error = e.message)
         }
 
     }
@@ -29,6 +69,18 @@ class SharedViewModel @Inject constructor(
         super.onCleared()
         println("ViewModel cleared")
     }
+}
+
+
+sealed interface SummaryEvent {
+    data class GetSummary(val content: String) : SummaryEvent
+}
+
+sealed interface SummaryState {
+    object Idle : SummaryState
+    object Loading : SummaryState
+    data class Success(val summary: String) : SummaryState
+    data class Error(val message: String) : SummaryState
 }
 
 data class SharedArticleState(
