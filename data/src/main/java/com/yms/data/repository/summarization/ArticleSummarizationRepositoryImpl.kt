5bb66290e092ba29716b890f1aa5cdad469a6fe4
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-01-03 19:37:06 +0300

    feat: implementation get summary method

---
+++ data/src/main/java/com/yms/data/repository/summarization/ArticleSummarizationRepositoryImpl.kt
@@ -0,0 +1,38 @@
+package com.yms.data.repository.summarization
+
+
+import android.content.Context
+import com.google.ai.client.generativeai.type.content
+import com.yms.data.R
+import com.yms.data.mapper.NewsMapper.toArticleSummary
+import com.yms.data.remote.dto.gemini.ArticleSummaryDto
+import com.yms.data.utils.generativeModel
+import com.yms.domain.model.gemini.ArticleSummary
+import com.yms.domain.repository.summarization.ArticleSummarizationRepository
+import com.yms.domain.utils.RootResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class ArticleSummarizationRepositoryImpl(val context: Context) : ArticleSummarizationRepository {
+
+    override fun getSummary(content: String): Flow<RootResult<ArticleSummary>> = flow {
+        emit(RootResult.Loading)
+
+        val response = generativeModel.generateContent(
+            content {
+                text("${context.getString(R.string.prompt_summary)} $content")
+            }
+        ).text ?: context.getString(R.string.error_summary)
+
+        val articleSummary = ArticleSummaryDto(response)
+
+        emit(RootResult.Success(articleSummary.toArticleSummary()))
+
+    }.flowOn(Dispatchers.IO)
+        .catch {
+            emit(RootResult.Error(it.message ?: "Something went wrong"))
+        }
+}
