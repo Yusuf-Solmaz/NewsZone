04f64c5de7e6717f19709be4fa3e3c6673e5d829
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-01-04 14:53:34 +0300

    feat: add image parameter to Page

--- presentation/src/main/java/com/yms/presentation/onboarding/Page.kt
+++ presentation/src/main/java/com/yms/presentation/onboarding/Page.kt
@@ -1,11 +1,13 @@
 package com.yms.presentation.onboarding
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.yms.presentation.R
 
 data class Page(
     @StringRes val title: Int,
-    @StringRes val description: Int
+    @StringRes val description: Int,
+    @DrawableRes val image: Int
 )
 
 fun getOnboardingPages(): List<Page> {
@@ -13,14 +15,17 @@ fun getOnboardingPages(): List<Page> {
         Page(
             title = R.string.onboarding_title_1,
             description = R.string.onboarding_description_1,
+            image = R.drawable.image_onboarding_1
         ),
         Page(
             title = R.string.onboarding_title_2,
             description = R.string.onboarding_description_2,
+            image = R.drawable.image_onboarding_2
         ),
         Page(
             title = R.string.onboarding_title_3,
             description = R.string.onboarding_description_3,
+            image = R.drawable.image_onboarding_3
         )
     )
 }
