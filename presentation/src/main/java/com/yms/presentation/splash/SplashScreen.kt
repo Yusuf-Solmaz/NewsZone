bda761738c2d1f8429286ad0dc2e16e6d627d65c
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-01-04 21:42:45 +0300

    feat: add splash screen image

--- presentation/src/main/java/com/yms/presentation/splash/SplashScreen.kt
+++ presentation/src/main/java/com/yms/presentation/splash/SplashScreen.kt
@@ -1,15 +1,22 @@
 package com.yms.presentation.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.yms.presentation.R
 import kotlinx.coroutines.delay
 
 @Composable
@@ -23,11 +30,20 @@ fun SplashScreen(modifier: Modifier = Modifier, navigateToScreen: () -> Unit) {
     Column(
         modifier = modifier
             .fillMaxSize()
-            .background(Color.Black),
+            .background(MaterialTheme.colorScheme.background),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Splash Screen", color = Color.White)  // Ekranda yazıyı göster
+        Image(
+            painter = painterResource(R.drawable.image_splash),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(R.string.splash_title),
+            style = MaterialTheme.typography.titleSmall,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+
     }
 
 }
