eb33f839075d3fa1e09fe8f13d92fce25c1d9434
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-01-06 11:13:28 +0300

    feat: add GoToUrl and ShareArticle data classes

--- presentation/src/main/java/com/yms/presentation/article_detail/ArticleDetailEvent.kt
+++ presentation/src/main/java/com/yms/presentation/article_detail/ArticleDetailEvent.kt
@@ -6,4 +6,6 @@ sealed interface ArticleDetailEvent {
     data class InsertArticle(val article: BaseArticle) : ArticleDetailEvent
     data class IsArticleSaved(val articleUrl: String) : ArticleDetailEvent
     data class DeleteArticle(val articleUrl: String) : ArticleDetailEvent
+    data class GoToUrl(val url: String?) : ArticleDetailEvent
+    data class ShareArticle(val articleTitle: String, val articleUrl: String?) : ArticleDetailEvent
 }
