c5f3cb255320e891608d32b9e7d9e5b5022af7b2
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-01-06 11:14:39 +0300

    feat: add general loading lottie

---
+++ presentation/src/main/java/com/yms/utils/LoadingLottie.kt
@@ -0,0 +1,23 @@
+package com.yms.utils
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.yms.presentation.R
+
+@Composable
+fun LoadingLottie(resId:Int = R.raw.loading, height: Dp = 300.dp){
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(resId))
+    LottieAnimation(
+        composition = composition,
+        iterations = LottieConstants.IterateForever,
+        modifier = Modifier.height(height)
+    )
+}
