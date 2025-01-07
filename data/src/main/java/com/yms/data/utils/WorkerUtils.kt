801b3725402c3113893ca7207cd5366888f77a5f
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-01-07 14:10:33 +0300

    feat: add required constants

--- data/src/main/java/com/yms/data/utils/WorkerUtils.kt
+++ data/src/main/java/com/yms/data/utils/WorkerUtils.kt
@@ -12,6 +12,11 @@ import androidx.core.content.ContextCompat
 import com.yms.domain.R
 import com.yms.domain.model.news.ArticleData
 
+const val FOLLOW_UP_TIME_KEY = "follow_up_time"
+const val AGE_GROUP_KEY = "age_group"
+const val GENDER_KEY = "gender"
+const val WORKER_ERROR = "worker_error"
+const val OUTPUT_CATEGORY_KEY = "output_category_key"
 
 fun makeStatusNotification(articleData: ArticleData, context: Context) {
 
