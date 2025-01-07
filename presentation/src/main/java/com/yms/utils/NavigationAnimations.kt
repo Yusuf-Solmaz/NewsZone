036d53041150af9cb795585107331884abd8d711
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-01-07 14:12:11 +0300

    feat: add navigation animation

---
+++ presentation/src/main/java/com/yms/utils/NavigationAnimations.kt
@@ -0,0 +1,21 @@
+package com.yms.utils
+
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
+import androidx.navigation.NavBackStackEntry
+
+fun slideInToRight(scope: AnimatedContentTransitionScope<NavBackStackEntry>): EnterTransition {
+    return scope.slideIntoContainer(
+        AnimatedContentTransitionScope.SlideDirection.Right,
+        animationSpec = tween(300)
+    )
+}
+
+fun slideOutToLeft(scope: AnimatedContentTransitionScope<NavBackStackEntry>): ExitTransition {
+    return scope.slideOutOfContainer(
+        AnimatedContentTransitionScope.SlideDirection.Left,
+        animationSpec = tween(300)
+    )
+}
